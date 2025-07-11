0638c412d3a7433fd41eb8e67e645bc990abaf5c
momys1 <universitasindo.id00010@gmail.com>
2025-07-11 11:52:34 +0000

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+echo -ne '>>>                       [20%]\r'
+apt install sudo -y > /dev/null 2>&1
+sleep 2
+echo -ne '>>>>>>>                   [40%]\r'
+apt install curl -y > /dev/null 2>&1
+sleep 2
+echo -ne '>>>>>>>>>>>>>>            [60%]\r'
+apt wget -y > /dev/null 2>&1
+sleep 2
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>   [80%]\r'
+apt update -y > /dev/null 2>&1
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
+echo -ne '\n'
